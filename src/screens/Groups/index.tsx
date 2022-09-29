ff9b2df13163a61f21e55201cb33307f172a6e37
Otavio Martins <otaviomartinslima@gmail.com>
2022-09-29 09:26:25 -0300

    first commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,77 @@
+
+import { useState, useCallback } from 'react';
+import { Alert, FlatList } from 'react-native';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { Container } from './styles';
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
+import { groupsGetAll } from '@storage/group/groupsGetAll';
+import { Loading } from '@components/Loading';
+
+export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigation = useNavigation();
+
+  function handleNewGroup(){
+    navigation.navigate('new');
+  }
+
+  async function fetchGroups(){
+    try{
+      setIsLoading(true);
+      const data = await groupsGetAll();
+      setGroups(data);
+      setIsLoading(false);
+
+    } catch(error)  {
+      console.log(error);
+      Alert.alert('Turmas', 'Não foi possível carregar as turmas');
+    }
+  }
+
+  function handleOpenGroup(group: string){
+    navigation.navigate('players', {group});
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups();
+  },[]));
+
+  return (
+    <Container>
+        <Header/>
+
+        <Highlight
+        title="Turmas"
+        subtitle="jogue com a sua turma"
+        />
+        { isLoading ? <Loading/>:
+      <FlatList
+      data={groups}
+      keyExtractor={item => item}
+      renderItem={({item }) => (
+      <GroupCard
+       title={item}
+       onPress={() => handleOpenGroup(item)}
+       />
+       )}
+       contentContainerStyle={groups.length === 0 && { flex: 1}}
+       ListEmptyComponent={() => (
+        <ListEmpty
+          message="Que tal cadastrar a primeira turma?"
+        /> )}
+        showsVerticalScrollIndicator={false}
+      />
+       }
+      <Button 
+        title="Criar nova turma"
+        onPress={handleNewGroup}
+      />
+    </Container>
+  );
+}
+
